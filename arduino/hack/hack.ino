2329a24d74d6ed552d3e5490718f07a010d546ea
alanboy18 <s100096.tw@gmail.com>
2018-05-05 22:02:44 +0800

    201805051002PM

--- arduino/hack/hack.ino
+++ arduino/hack/hack.ino
@@ -1,18 +1,23 @@
 int count,trig;
 long adc_value,adc_value_last;
 #define trig_pin 2
+#define bz 9
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
   pinMode(trig_pin,OUTPUT);
+  pinMode(bz,OUTPUT);
   pinMode(7,INPUT);
   pinMode(8,OUTPUT);
   pinMode(A0,INPUT);
+  pinMode(13,OUTPUT);
   
   digitalWrite(7,1);
-  digitalWrite(8,1);  
+  digitalWrite(8,1);
+  digitalWrite(13,0);
+  digitalWrite(trig_pin,0);  
 }
 
 void loop() {
@@ -23,6 +28,7 @@ void loop() {
       count=0;
 //      Serial.println(analogRead(A0));
       adc_value=analogRead(A0);
+      
       delay(50);
       if(adc_value_last<30 && adc_value>=60)
       {        
@@ -36,9 +42,13 @@ void loop() {
           trig=0;
           Serial.print("HAHA   ");
           Serial.println(adc_value);
+          analogWrite(bz,100);
           digitalWrite(trig_pin,1);
-          delay(10);
+          digitalWrite(13,1);
+          delay(50);
+          analogWrite(bz,0);
           digitalWrite(trig_pin,0);
+          digitalWrite(13,0);
         break;
       }
     break;
